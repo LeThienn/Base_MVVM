cafa5d29a46b916c799d39ad2efe790457e52799
LeThienn <55796074+LeThienn@users.noreply.github.com>
2022-06-20 22:06:39 +0700

    update splash

--- lib/src/presentation/splash/splash_viewmodel.dart
+++ lib/src/presentation/splash/splash_viewmodel.dart
@@ -4,9 +4,8 @@ import 'package:get/get.dart';
 import '../presentation.dart';
 
 class SplashViewModel extends BaseViewModel {
-
   init() async {
-    EasyLoading.show(status: 'loading...');
+    EasyLoading.show(status: 'loading........');
     EasyLoading.dismiss();
     Get.toNamed(Routers.navigation);
   }
